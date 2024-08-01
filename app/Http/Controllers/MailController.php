8d5159757795ae96b7e806930a3ed47fb32c6e21
mohamed khaled <mohamedkhaledx12@gmail.com>
2024-08-01 13:31:53 +0300

    1/8/2024
    fixed not being able to subscribe if not logged in

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,7 +10,7 @@ class MailController extends Controller
 {
     function __construct()
     {
-        $this->middleware('permission:Admin');
+        $this->middleware('permission:Admin')->except('store');
     }
     /**
      * Display a listing of the resource.
